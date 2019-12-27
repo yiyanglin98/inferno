33ccf2f185e4d822354957e374a2617c8f8a2aa2
davidlin <davidlinyiyang@gmail.com>
2019-12-26 23:15:24 -0600

    update license

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject inferno "0.1.0-SNAPSHOT"
   :description "A text-based adventure game built using Clojure."
   :url "https://github.com/yiyanglin98/inferno"
-
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns inferno.core}
   :main ^:skip-aot inferno.core/main
